cd7a9ba5faf247820c3d79768af918a8c76e9bcd
Zeno Jiricek <airtonix@users.noreply.github.com>
2024-10-05 11:23:08 +0930

    fix test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-asdf plugin test dokku https://github.com/airtonix/asdf-dokku.git "dokku-client"
+asdf plugin test dokku https://github.com/airtonix/asdf-dokku-client.git "dokku-client"
